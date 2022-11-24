d5073d7ae50bc095cc844f569093de8ede48b391
Jaseela.A <jaseelachittur@gmail.com>
2022-11-24 10:39:42 +0530

    added number input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,37 @@
 import { StyleSheet, Text, View ,TextInput} from 'react-native'
 import React from 'react'
+import CustomButton from '../components/CustomButton'
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput/>
+    <View style={styles.startContainer}>
+      <TextInput style={styles.textInput} maxLength={2} keyboardType='number-pad' autoCapitalize="none"/>
+      <CustomButton>Reset</CustomButton>
+      <CustomButton>Start</CustomButton>
     </View>
   )
 }
 
 export default StartGameScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  startContainer:{
+    padding:24,
+    backgroundColor:"#8fbc8f",
+    marginTop:24,
+    marginHorizontal:16,
+    borderRadius:8,
+    elevation:6 //android specific property for shadow
+  },
+  textInput:{
+    height:70,
+    fontSize:32,
+    borderBottomColor:"#ffe4e1",
+    borderBottomWidth:2,
+    color:"#ffe4e1",
+    marginVertical:8,
+    width:50,
+    textAlign:"center",
+    fontWeight:"bold"
+  }
+})
