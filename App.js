a21136036f384c4018c1926aede263cf3b2b52c8
Jaseela.A <jaseelachittur@gmail.com>
2022-11-28 18:23:01 +0530

    added splash screen

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView} from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import ResultScreen from "./screens/ResultScreen";
-import {useFonts} from "expo-font"
-import AppLoading from "expo-app-loading"
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
-
   const [number, setNumber] = useState();
   const [gameOver, setGameOver] = useState(true);
-  const [rounds,setRounds]=useState(0)
+  const [rounds, setRounds] = useState(0);
 
-  const [fontsLoaded]=useFonts({
-    'open-sans':require("./assets/fonts/OpenSans-Regular.ttf"),
-    'open-sans-bold':require("./assets/fonts/OpenSans-Bold.ttf"),
-  })
+  SplashScreen.preventAutoHideAsync();
 
-  if(!fontsLoaded){
-    return <AppLoading/>
+  const [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
   }
   const numberPickedHandler = (pickedNumber) => {
     setNumber(pickedNumber);
@@ -27,31 +34,37 @@ export default function App() {
   };
   const gameOverHandler = (numberOfRounds) => {
     setGameOver(true);
-    setRounds(numberOfRounds)
+    setRounds(numberOfRounds);
   };
-  const newGameHandler=()=>{
-    setNumber(null)
-    setRounds(0)
-  }
+  const newGameHandler = () => {
+    setNumber(null);
+    setRounds(0);
+  };
   let screen = <StartGameScreen numberPickedHandler={numberPickedHandler} />;
   if (number) {
     screen = <GameScreen number={number} gameOverHandler={gameOverHandler} />;
   }
   if (gameOver && number) {
-    screen = <ResultScreen number={number} rounds={rounds} newGameHandler={newGameHandler}/>;
+    screen = (
+      <ResultScreen
+        number={number}
+        rounds={rounds}
+        newGameHandler={newGameHandler}
+      />
+    );
   }
 
   return (
-    <LinearGradient style={styles.container} colors={["#d8bfd8", "#ffb6c1"]}>
-      <ImageBackground
-        source={require("./assets/images/background.jpg")}
-        resizeMode="cover"
-        style={styles.container}
-        imageStyle={styles.background}
-      >
-        <SafeAreaView style={styles.container}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+      <LinearGradient style={styles.container}  onLayout={onLayoutRootView} colors={["#d8bfd8", "#ffb6c1"]}>
+        <ImageBackground
+          source={require("./assets/images/background.jpg")}
+          resizeMode="cover"
+          style={styles.container}
+          imageStyle={styles.background}
+        >
+          <SafeAreaView style={styles.container}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
   );
 }
 
