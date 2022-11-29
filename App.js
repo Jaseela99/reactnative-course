2d7b6fbc61fb7b16370024a2c21229b1f39156d1
Jaseela.A <jaseelachittur@gmail.com>
2022-11-29 12:41:11 +0530

    added tab navigation

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import{NavigationContainer} from "@react-navigation/native"
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+import {Ionicons} from "@expo/vector-icons"
+const Tab=createBottomTabNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator screenOptions={{
+       headerStyle: { backgroundColor:"#9932cc" },
+       headerTintColor:"white",
+       tabBarActiveTintColor:"#ffb6c1",
+       tabBarActiveBackgroundColor:"#9932cc"
+    }}>
+      <Tab.Screen name="welcome" component={WelcomeScreen}
+       options={{
+        tabBarIcon:({color})=><Ionicons name="home" color={color} size={24}/>
+       }}/>
+      <Tab.Screen name="user" component={UserScreen}
+      options={{
+        tabBarIcon:({color})=><Ionicons name="person" color={color} size={24}/>
+       }}/>
+    </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
