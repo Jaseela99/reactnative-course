d59f78cc77eaa25542adbd070e4298974a866299
Jaseela.A <jaseelachittur@gmail.com>
2022-11-30 13:23:56 +0530

    added navigation and screens

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import AllExpenses from './screens/AllExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+import ManageExpense from './screens/ManageExpense';
+const Stack= createNativeStackNavigator()
+const Tab= createBottomTabNavigator()
 
+const ViewExpenses=()=>{
+  return(<Tab.Navigator>
+    <Tab.Screen name="AllExpenses" component={AllExpenses}/>
+    <Tab.Screen name="RecentExpenses" component={RecentExpenses}/>
+   </Tab.Navigator>)
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+         <Stack.Navigator initialRouteName="ViewExpenses">
+          <Stack.Screen name="ViewExpenses" component={ViewExpenses}/>
+          <Stack.Screen name="ManageExpense" component={ManageExpense}/>
+         </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
