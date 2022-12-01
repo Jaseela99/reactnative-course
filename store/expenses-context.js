a8d2b6d51b6ec1d4a2ea5b99edff273c5dfab633
Jaseela.A <jaseelachittur@gmail.com>
2022-12-01 11:37:25 +0530

    added some comments

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -28,23 +28,24 @@ const DUMMY_EXPENSES=[
     },
 ]
 
-export const ExpensesContext=createContext({
+export const ExpensesContext=createContext({      //initial Context
     expenses:[],
     addExpense:({description,amount,date})=>{},
     updateExpense:(id)=>{},
     deleteExpense:(id,{description,amount,date})=>{}
     })
 const expensesReducer=(state,action)=>{
+    //reducer takes in state and action and returns new state from action
   switch(action.type) {
     case "ADD":
         const id= new Date().toString() + Math.random().toString()
         return [{...action.payload,id:id},...state]
     case "UPDATE":
-        const updatableExpenseIndex=state.findIndex((expense)=>expense.id===action.payload.id)
+        const updatableExpenseIndex=state.findIndex((expense)=>expense.id===action.payload.id) //findIndex is a method to find an element in an array
         const updatableExpense= state[updatableExpenseIndex]
-        const updatedItem={...updatableExpense,...action.payload.data}
+        const updatedItem={...updatableExpense,...action.payload.data}//merging the new data with the existing one
         const updatedExpenses=[...state]
-        updatedExpenses[updatableExpenseIndex]=updatedItem
+        updatedExpenses[updatableExpenseIndex]=updatedItem//adding it to the array
         return updatedExpenses
     case "DELETE":
         return state.filter((expense)=>expense.id!==action.payload)
@@ -54,9 +55,12 @@ const expensesReducer=(state,action)=>{
 }
 
 const ExpensesContextProvider=({children})=>{
+    //useReducer takes in reducer and initial state
+    //state managed by reducer and dispatch the action accordingly
+    //when the app renders for the first time the expenses will not be empty its value will be dummy expenses
    const [expensesState,dispatch]= useReducer(expensesReducer,DUMMY_EXPENSES)
-
    const addExpense=(expenseData)=>{
+    //dispatching action which is deconstructed as type and payload
     dispatch({type:"ADD",payload:expenseData})
    }
    const deleteExpense=(id)=>{
