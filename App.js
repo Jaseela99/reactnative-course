cebf3646791e58875032ed31fa21512849f9739f
Jaseela.A <jaseelachittur@gmail.com>
2022-12-02 14:50:24 +0530

    added basic setup

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import { GlobalStyles } from "./constants/styles";
+
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary450 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: GlobalStyles.colors.primary150 },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignUpScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AuthenticatedStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary450 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: GlobalStyles.colors.primary150 },
+      }}
+    >
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+
+      <Navigation />
+    </>
+  );
+}
---
+++ constants/styles.js
@@ -0,0 +1,21 @@
+export const GlobalStyles={
+    colors:{
+    primary50:"#ffdab9",
+    primary100:"#ffe4e1",
+    primary150:"#ffa07a",
+    primary200:"#dc143c",
+    primary250:"#b22222",
+    primary300:"#a52a2a",
+    primary350:"#cd5c5c",
+    primary400:"#8b0000",
+    primary450:"#800000",
+    primary500:"#bc8f8f",
+    gray50:"#c0c0c0",
+    gray100:"#696969",
+    white:"#ffff",
+    selected50:"#fafad2",
+    selected100:"#ffe4c4",
+    error50:"#ff0000",
+
+    }
+}
