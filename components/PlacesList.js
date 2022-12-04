38b18ccc804d2b1e57543b00e7a0fdc426aa7592
Jaseela.A <jaseelachittur@gmail.com>
2022-12-04 11:49:06 +0530

    added basic screens and components

---
+++ components/PlacesList.js
@@ -0,0 +1,31 @@
+import { StyleSheet,FlatList } from 'react-native'
+import React from 'react'
+import PlaceItem from './PlaceItem'
+
+const PlacesList = ({places}) => {
+    if(!places||places.length===0){
+        return <View style={styles.fallbackContainer}>
+            <Text style={styles.fallbackText}>No Places added Yet</Text>
+        </View>
+    }
+
+    const renderPlaces=({item})=>{
+    return <PlaceItem place={item}/>
+    }
+  return (
+    <FlatList data={places} keyExtractor={(itemData)=>itemData.id} renderItem={renderPlaces}/>
+  )
+}
+
+export default PlacesList
+
+const styles = StyleSheet.create({
+    fallbackContainer:{
+       flex:1,
+       justifyContent:"center",
+       alignItems:"center" 
+    },
+    fallbackText:{
+        fontSize:16
+    }
+})
