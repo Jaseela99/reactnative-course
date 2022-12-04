432da46b71a8d62698f368102054b2e7bb5b7243
Jaseela.A <jaseelachittur@gmail.com>
2022-12-04 10:17:23 +0530

    added modifications

--- App.js
+++ App.js
@@ -7,12 +7,10 @@ import SignUpScreen from "./screens/SignUpScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import { GlobalStyles } from "./constants/styles";
 import AuthContextProvider, { AuthContext } from "./store/auth-context";
-import { useContext, useEffect, useState, useCallback } from "react";
+import { useContext, useEffect } from "react";
 import IconButton from "./components/UI/IconButton";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as SplashScreen from "expo-splash-screen";
 const Stack = createNativeStackNavigator();
-SplashScreen.preventAutoHideAsync();
 function AuthStack() {
   return (
     <Stack.Navigator
@@ -69,7 +67,6 @@ function Navigation() {
 }
 
 const Root = () => {
-  const [appIsReady, setAppIsReady] = useState(false);
   const authContext = useContext(AuthContext);
   useEffect(() => {
     const fetchToken = async () => {
@@ -77,20 +74,11 @@ const Root = () => {
       if (storedToken) {
         authContext.authenticate(storedToken);
       }
-      setAppIsReady(false);
     };
     fetchToken();
   }, []);
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-  return <Navigation onLayout={onLayoutRootView} />;
+  
+  return <Navigation/>;
 };
 export default function App() {
   return (
